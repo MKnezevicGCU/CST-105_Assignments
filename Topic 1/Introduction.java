2ca0fd6cea2b166df90c32a124d4c47afa61133b
DESKTOP-FS7QN9L\XKyoj <MKnezevic@my.gcu.edu>
2018-04-08 16:02:09 -0700

    This is my work and I hope you like it.

---
+++ Topic 1/Introduction.java
@@ -0,0 +1,8 @@
+// This is Michael Knezevic and this is my own work.
+public class Introduction {
+	public static void main(String[] Args) {
+		System.out.println("My name is Michael Knezevic");
+		System.out.println("My favorite color is purple");
+		System.out.println("If I won the lottery I would help my parents as they have helped me");
+	}
+}
